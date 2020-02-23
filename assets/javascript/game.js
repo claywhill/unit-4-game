c2532f708075c7914c4d60851f5509478133e92c
Clayton Hill <clayman0123@yahoo.com>
2020-02-23 09:54:34 -0700

    random numbers working and buttons working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,4 +5,38 @@
 // If user adds up buttons above computer number then increase losses
 
 
-$()
+$(".random").ready(function() {
+   var computerNum = [Math.floor(Math.random() * (120 - 19 + 1)) + 19];
+  $(".random").text(computerNum);
+});
+
+$(".button1").ready(function() {
+    var buttonNum1= [Math.floor(Math.random() * (3 - 1 + 1)) + 1];
+    $(".button1").click(function() {
+        $(".score").text(buttonNum1);
+    });
+});
+
+$(".button2").ready(function() {
+    var buttonNum2= [Math.floor(Math.random() * (6 - 4 + 1)) + 4];
+    $(".button2").click(function() {
+        $(".score").text(buttonNum2);
+    });
+});
+
+$(".button3").ready(function() {
+    var buttonNum3= [Math.floor(Math.random() * (9 - 7 + 1)) + 7];
+    $(".button3").click(function() {
+        $(".score").text(buttonNum3);
+    });
+});
+
+$(".button4").ready(function() {
+    var buttonNum4= [Math.floor(Math.random() * (12 - 10 + 1)) + 10];
+    $(".button4").click(function() {
+        $(".score").text(buttonNum4);
+    });
+});
+
+
+
