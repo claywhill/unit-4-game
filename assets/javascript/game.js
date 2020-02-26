76779bedf1e31106b89f0470cab34ea4c5347b08
Clayton Hill <clayman0123@yahoo.com>
2020-02-26 10:05:48 -0700

    buttons now add up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,39 +4,62 @@
 // If user is able to add buttons up to computer number then increase wins
 // If user adds up buttons above computer number then increase losses
 
+var wins = 0;
 
-$(".random").ready(function() {
-   var computerNum = [Math.floor(Math.random() * (120 - 19 + 1)) + 19];
-  $(".random").text(computerNum);
+var losses = 0;
+
+var computerNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+
+var buttonNum1 = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+var buttonNum2 = Math.floor(Math.random() * (6 - 4 + 1)) + 4;
+
+var buttonNum3 = Math.floor(Math.random() * (9 - 7 + 1)) + 7;
+
+var buttonNum4 = Math.floor(Math.random() * (12 - 10 + 1)) + 10;
+
+var buttonValues = [];
+
+function runningTotal() {
+  var total = 0;
+  for (var i = 0; i < buttonValues.length; i++) {
+    total = (total + buttonValues[i]) << 0;
+  }
+
+  $(".score").text(total);
+}
+
+$(".random").text(computerNum);
+
+$(".button1").click(function() {
+  buttonValues.push(buttonNum1);
+  $(".score").text(buttonValues);
+  runningTotal();
 });
 
-$(".button1").ready(function() {
-    var buttonNum1= [Math.floor(Math.random() * (3 - 1 + 1)) + 1];
-    $(".button1").click(function() {
-        $(".score").text(buttonNum1);
-    });
+$(".button2").click(function() {
+  buttonValues.push(buttonNum2);
+  $(".score").text(buttonValues);
+  runningTotal();
 });
 
-$(".button2").ready(function() {
-    var buttonNum2= [Math.floor(Math.random() * (6 - 4 + 1)) + 4];
-    $(".button2").click(function() {
-        $(".score").text(buttonNum2);
-    });
+$(".button3").click(function() {
+  buttonValues.push(buttonNum3);
+  $(".score").text(buttonValues);
+  runningTotal();
 });
 
-$(".button3").ready(function() {
-    var buttonNum3= [Math.floor(Math.random() * (9 - 7 + 1)) + 7];
-    $(".button3").click(function() {
-        $(".score").text(buttonNum3);
-    });
+$(".button4").click(function() {
+  buttonValues.push(buttonNum4);
+  $(".score").text(buttonValues);
+  runningTotal();
 });
 
-$(".button4").ready(function() {
-    var buttonNum4= [Math.floor(Math.random() * (12 - 10 + 1)) + 10];
-    $(".button4").click(function() {
-        $(".score").text(buttonNum4);
-    });
-});
-
-
-
+if (total == computerNum) {
+  wins++;
+  $(".wins").text("Wins: " + wins);
+}
+else if (total > computerNum) {
+  losses++;
+  $(".losses").text("Losses: " + losses);
+};
